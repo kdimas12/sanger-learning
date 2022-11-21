e873110b4fb21abc4878a5344295d019aa621535
Dimas <dimaskurniawan40@gmail.com>
2022-11-21 15:07:13 +0700

    :bug: Fix bug get kode pendaftaran

--- app/Models/PendaftaranKelasModel.php
+++ app/Models/PendaftaranKelasModel.php
@@ -8,18 +8,19 @@ class PendaftaranKelasModel extends Model
 {
     public function getKodePendaftaran()
     {
-        $query = $this->db->table('tbl_pendaftaran')->select('MAX(RIGHT(id_pendaftaran),3) as id', FALSE);
-        $kd = "";
-        if ($query->countAllResults() > 0) {
-            foreach ($query->get()->getResult() as $k) {
-                $tmp = ((int) $k->id_pendaftaran) + 1;
-                $kd = sprintf("%04s", $tmp);
-            }
+        $query = $this->db->table('tbl_pendaftaran')->select('RIGHT(id_pendaftaran,3) as id', FALSE)->where('tanggal_pendaftaran', date('Y-m-d'))->orderBy('id', 'DESC')->limit(1)->get();
+
+        $kode = "";
+        if ($query->resultID->num_rows <> 0) {
+            $data = $query->getRow();
+            $kode = intval($data->id) + 1;
         } else {
-            $kd = "001";
+            $kode = 1;
         }
-        date_default_timezone_set('Asia/Jakarta');
-        return "PK" . date('dmy') . $kd;
+        $tgl = date('dmy');
+        $batas = str_pad($kode, 3, "0", STR_PAD_LEFT);
+        $kodetampil = "PK" . $tgl . $batas;  //format kode
+        return $kodetampil;
     }
 
     public function getPendaftaranKelas()
