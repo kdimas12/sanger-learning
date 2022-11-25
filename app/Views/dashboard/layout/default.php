a62249884b7950eba44f96d1dc650609f8cce91d
Dimas <dimaskurniawan40@gmail.com>
2022-11-25 19:16:11 +0700

    :art: Make Dashboard to view partials

---
+++ app/Views/dashboard/layout/default.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?= $this->include('dashboard/layout/head') ?>
+
+<body class="sb-nav-fixed">
+    <?= $this->include('dashboard/layout/navbar') ?>
+    <div id="layoutSidenav">
+        <?= $this->include('dashboard/layout/sidebar') ?>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <?= $this->renderSection('content') ?>
+                </div>
+            </main>
+            <?= $this->include('dashboard/layout/footer') ?>
+        </div>
+    </div>
+    <script src="<?= base_url('assets/js/bootstrap.bundle.min.js') ?>" crossorigin="anonymous"></script>
+    <script src="<?= base_url('assets/js/sidebar-sbadmin.js') ?>"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="<?= base_url('assets/js/datatables-simple-demo.js') ?>"></script>
+</body>
+
+</html>
