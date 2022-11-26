fb29809dd0363c1a64e6fb36b01dff79ddc46939
Dimas <dimaskurniawan40@gmail.com>
2022-11-26 23:31:43 +0700

    :wrench: Add namespace datatables

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -41,8 +41,10 @@ class Autoload extends AutoloadConfig
      * @var array<string, string>
      */
     public $psr4 = [
-        APP_NAMESPACE => APPPATH, // For custom app namespace
-        'Config'      => APPPATH . 'Config',
+        APP_NAMESPACE           => APPPATH, // For custom app namespace
+        'Config'                => APPPATH . 'Config',
+        'PHPSQLParser'          => APPPATH . 'ThirdParty/php-sql-parser/src/PHPSQLParser', // <-- namespace for php-sql-parser
+        'Hermawan\DataTables'   => APPPATH . 'ThirdParty/codeigniter4-datatables/src', // <-- namespace for this library
     ];
 
     /**
