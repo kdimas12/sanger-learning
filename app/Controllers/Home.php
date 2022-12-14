5e227c93c5b5b3a1fe8f6eb65e1e2cbddaa1c756
Dimas <dimaskurniawan40@gmail.com>
2022-12-15 01:24:57 +0700

    :safety_vest: Add validation for Konfirmasi

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Models\KonfirmasiModel;
+use App\Models\UserModel;
+
 class Home extends BaseController
 {
+	public function __construct()
+	{
+		$this->konfirmasiModel = new KonfirmasiModel();
+		$this->userModel = new UserModel();
+	}
+
 	public function index()
 	{
-		return view('welcome_message');
+		$session = session();
+		$data['konfirmasi'] = "";
+		if ($session->id_user != null) {
+			$id_user = $session->id_user;
+			$id_pendaftaran = $this->userModel->find($id_user)['id_pendaftaran'];
+			$data['konfirmasi'] = $this->konfirmasiModel->where('id_pendaftaran', $id_pendaftaran)->findAll();
+		}
+		return view('welcome_message', $data);
 	}
 }
