54510be0a72d9aaba8f36be8b6acb48c3803f611
Dimas <dimaskurniawan40@gmail.com>
2022-12-23 00:44:34 +0700

    :hammer: Add data konfirmasi

--- app/Controllers/Invoice.php
+++ app/Controllers/Invoice.php
@@ -3,11 +3,20 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\KonfirmasiModel;
 
 class Invoice extends BaseController
 {
-    public function index()
+    public function __construct()
     {
-        return view('invoice');
+        $this->konfirmasi = new KonfirmasiModel();
+    }
+
+    public function index($id_konfirmasi)
+    {
+        $dataKonfirmasi = $this->konfirmasi->getKonfirmasiData($id_konfirmasi)->getResultArray();
+
+        $data['konfirmasi'] = $dataKonfirmasi[0];
+        return view('invoice', $data);
     }
 }
