c40ea7fd2bc44f515643a57af9c3b7ae93e6a50b
Dimas <dimaskurniawan40@gmail.com>
2022-12-23 02:27:42 +0700

    :bug: Fix error getKodeBarang

--- app/Models/PendaftaranKelasModel.php
+++ app/Models/PendaftaranKelasModel.php
@@ -16,12 +16,12 @@ class PendaftaranKelasModel extends Model
 
     public function getKodePendaftaran()
     {
-        $query = $this->db->table('tbl_pendaftaran')->select('RIGHT(id_pendaftaran,3) as id', FALSE)->where('tanggal_pendaftaran', date('Y-m-d'))->orderBy('id', 'DESC')->limit(1)->get();
+        $query = $this->db->table('tbl_pendaftaran')->select('RIGHT(id_pendaftaran, 2) as id_pendaftaran')->orderBy('id_pendaftaran', 'DESC')->limit(1)->get();
 
         $kode = "";
         if ($query->resultID->num_rows <> 0) {
             $data = $query->getRow();
-            $kode = intval($data->id) + 1;
+            $kode = intval($data->id_pendaftaran) + 1;
         } else {
             $kode = 1;
         }
