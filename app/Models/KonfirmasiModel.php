6171697dc15b0b18371902aba18180b880d6f4cb
Dimas <dimaskurniawan40@gmail.com>
2022-12-23 00:43:57 +0700

    :hammer: Add function to get all data related to Konfirmasi

--- app/Models/KonfirmasiModel.php
+++ app/Models/KonfirmasiModel.php
@@ -20,4 +20,10 @@ class KonfirmasiModel extends Model
     {
         return 'KONF' . Date('ymds');;
     }
+
+    public function getKonfirmasiData($id_konfirmasi)
+    {
+        $query = $this->db->table('tbl_konfirmasi')->where('id_konfirmasi', $id_konfirmasi)->join('tbl_pendaftaran', 'tbl_konfirmasi.id_pendaftaran = tbl_pendaftaran.id_pendaftaran')->join('tbl_kelas', 'tbl_pendaftaran.id_kelas = tbl_kelas.id_kelas')->join('tbl_jenis_kelas', 'tbl_kelas.id_jenis = tbl_jenis_kelas.id_jenis')->limit(1)->get();
+        return $query;
+    }
 }
